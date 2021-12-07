f3226bf3edd77e0839c81beb90e4fa2599bd88a6
lennonos <lennon_3000@hotmail.com>
2021-12-07 16:33:31 -0300

    Move files to projetct folder

--- docker/database_and_grant.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE guacamole_db;
-CREATE USER 'guacamole_user'@'localhost' IDENTIFIED BY 'rafraf.123';
-GRANT SELECT,INSERT,UPDATE,DELETE ON guacamole_db.* TO 'guacamole_user'@'localhost';
-CREATE USER 'guacamole_user'@'172.19.0.4' IDENTIFIED BY 'rafraf.123';
-GRANT SELECT,INSERT,UPDATE,DELETE ON guacamole_db.* TO 'guacamole_user'@'172.19.0.4';
-FLUSH PRIVILEGES;
-EXIT;
