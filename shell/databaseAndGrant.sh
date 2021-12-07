d3aedc2ce8f769f544ad04fe5771902bb5e014dd
lennonos <lennon_3000@hotmail.com>
2021-12-07 01:50:38 +0000

    update into config bdMysqlConfig

--- shell/databaseAndGrant.sh
+++ shell/databaseAndGrant.sh
@@ -1,3 +1,6 @@
-mysql -u root -prafraf.123 < database_and_grant.sql
-
-mysql -u root -prafraf.123 guacamole_db < initdb.sql
+#Exec script into mysql
+docker exec -i bdMysql  mysql -u root -prafraf.123 < database_and_grant.sql
+#Generate database structure
+docker run --rm guacamole/guacamole /opt/guacamole/bin/initdb.sh --mysql > initdb.sql
+#Exec script into mysql
+docker exec -i bdMysql  mysql -u root -prafraf.123 guacamole_db < initdb.sql
