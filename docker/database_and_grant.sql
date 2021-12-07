18a1d36552b6e374c69fb61cc69d3099632a3d2d
lennonos <lennon_3000@hotmail.com>
2021-12-07 19:27:53 +0000

    Ready to deploy!

--- docker/database_and_grant.sql
+++ docker/database_and_grant.sql
@@ -1,5 +1,7 @@
 CREATE DATABASE guacamole_db;
 CREATE USER 'guacamole_user'@'localhost' IDENTIFIED BY 'rafraf.123';
 GRANT SELECT,INSERT,UPDATE,DELETE ON guacamole_db.* TO 'guacamole_user'@'localhost';
+CREATE USER 'guacamole_user'@'172.19.0.4' IDENTIFIED BY 'rafraf.123';
+GRANT SELECT,INSERT,UPDATE,DELETE ON guacamole_db.* TO 'guacamole_user'@'172.19.0.4';
 FLUSH PRIVILEGES;
-EXIT
+EXIT;
