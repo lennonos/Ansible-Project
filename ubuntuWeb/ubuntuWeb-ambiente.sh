762e193353dd8d9fb5b6ae63c3224e8bb99bb3ea
lennonos <lennon_3000@hotmail.com>
2022-07-28 17:38:22 -0300

    Script de configuracao

---
+++ ubuntuWeb/ubuntuWeb-ambiente.sh
@@ -0,0 +1,14 @@
+#Downloads e repositorios
+    sudo apt install software-properties-common
+    sudo add-apt-repository ppa:ondrej/php
+    sudo apt update
+    sudo wget https://docs.flexform.com.br/oracle-instantclient12.2-basic-12.2.0.1.0-1.x86_64.rpm
+    sudo wget  https://docs.flexform.com.br/oracle-instantclient12.2-devel-12.2.0.1.0-1.x86_64.rpm
+    sudo wget  https://docs.flexform.com.br/oracle-instantclient12.2-sqlplus-12.2.0.1.0-1.x86_64.rpm
+
+
+#Pacotes necessarios
+    sudo apt -y install apache2
+    sudo apt -y install php7.4 php7.4-xml  php7.4-cli php7.4-common php7.4-fpm php7.4-json php7.4-opcache php7.4-readline libapache2-mod-php7.4 php-pear php7.4-dev
+    sudo apt -y alien
+    
